a0fcd181b00f474bc3df1d265eaec0b9d11a123f
anwarpatelnoori <anwarpatelrazvi>
2023-12-19 23:37:29 +0530

    link input and button

---
+++ script.js
@@ -0,0 +1,5 @@
+var get_vido_btn =document.getElementById("get-video-btn")
+get_vido_btn.addEventListener("click", (getVideo)=>{
+    let utube_link = document.getElementById('utube-link').value
+    console.log(utube_link)
+})

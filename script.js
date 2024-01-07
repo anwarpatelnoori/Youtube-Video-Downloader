4d6fa947d2c749bed1fa3ff5c27af7ed269057be
anwarpatelnoori <anwarpatelrazvi>
2024-01-07 20:58:37 +0530

    video title, detail and download section

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var get_vido_btn =document.getElementById("get-video-btn")
+var utube_full_data = document.querySelector('.utube-full-data')
 get_vido_btn.addEventListener("click", (getVideo)=>{
     let utube_link = document.getElementById('utube-link').value
     console.log(utube_link)
+    utube_full_data.style.display = 'flex';
 })
